d9e061e7966e64962b4a6aec848869f0fe773a30
Toma Morisawa <13.35.104@gmail.com>
2019-02-24 17:04:02 +0900

    update to use cloud functions

--- node-request.js
+++ node-request.js
@@ -1,34 +1,47 @@
-const postData = querystring.stringify({
-  'msg': 'Hello World!'
-});
+/**
+ * Responds to any HTTP request.
+ *
+ * @param {!express:Request} req HTTP request context.
+ * @param {!express:Response} res HTTP response context.
+ */
+exports.emojiHeaven = (req_, res_) => {
+  const querystring = require('querystring');
+  const https = require('https');
+  https.request.debug = true;
 
-const options = {
-  hostname: 'www.google.com',
-  port: 80,
-  path: '/upload',
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Content-Length': Buffer.byteLength(postData)
-  }
+  const postData = querystring.stringify({});
+
+  const options = {
+    hostname: 'slack.com',
+    port: 443,
+    path: '/api/reactions.add?token=' + process.env.SLACK_API_TOKEN + '&channel=CBJQUPCDU&timestamp=1550741144.000200&name=thumbsup',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    }
+  };
+
+  const req = https.request(options, (res) => {
+    console.log(`STATUS: ${res.statusCode}`);
+    console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+    res.setEncoding('utf8');
+    res.on('data', (chunk) => {
+      console.log(`BODY: ${chunk}`);
+    });
+    res.on('end', () => {
+      console.log('No more data in response.');
+    });
+  });
+
+  req.on('error', (e) => {
+    console.error(`problem with request: ${e.message}`);
+  });
+
+  // write data to request body
+  req.write('');
+  req.end();
+
+  let message = req_.query.message || req_.body.message || 'Hello World!';
+  res_.status(200).send(message);
+
 };
-
-const req = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
-  });
-  res.on('end', () => {
-    console.log('No more data in response.');
-  });
-});
-
-req.on('error', (e) => {
-  console.error(`problem with request: ${e.message}`);
-});
-
-// write data to request body
-req.write(postData);
-req.end();
