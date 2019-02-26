2c8c4c37c7472e5c0b0017d0196ca74c172fccea
Toma Morisawa <13.35.104@gmail.com>
2019-02-26 23:51:57 +0900

    update script: to get emoji short code

--- emoji-sentiment.js
+++ emoji-sentiment.js
@@ -1,9 +1,20 @@
 const emojiSentiment = require('emoji-sentiment');
+const emojiDataSource = require('emoji-datasource');
 
-const filtered = emojiSentiment.filter((element, index, array) => {
+const filtered = emojiSentiment.filter((element) => {
   return (element.score > 0.6);
 });
 
-const selected = filtered[Math.floor(Math.random() * filtered.length)];
+const get_short_name = () => {
+  while (1) {
+    const selected = filtered[Math.floor(Math.random() * filtered.length)].sequence;
+    const matched = emojiDataSource.filter((element) => {
+      return (element.unified == selected);
+    });
+    if (matched.length > 0) {
+      return matched[0].short_name;
+    }
+  }
+}
 
-console.log(selected.sequence);
+console.log(get_short_name());
